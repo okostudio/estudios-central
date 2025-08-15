fd81428cc3c782a90b01aeeb61d1b6c086cc1c76
okostudio <marek.suchanski@gmail.com>
2025-08-14 23:42:49 -0300

    pages are in place 2

--- app/(site)/estudios/page.tsx
+++ app/(site)/estudios/page.tsx
@@ -15,18 +15,16 @@ export default async function Studios() {
     return (
         <>
             <div className="container mx-auto px-4">
-                <div className="hero grid grid-cols-1 md:grid-cols-12 gap-2 mb-8">
+                <div className="gallery grid grid-cols-1 md:grid-cols-12 gap-2 mb-8">
                     {photos[0].galleryImages.map((image) => (
-                        console.log(image),
-                        <div key={image._key} className={image.width === 4 ? "col-span-4" : image.width === 8 ? "col-span-8" : "col-span-12"}>
-                            <Image
-                                width={image.asset.metadata.dimensions.width}
-                                height={image.asset.metadata.dimensions.height}
-                                src={image.asset.url}
-                                alt={`Gallery Image ${image._key}`}
-                                className="w-full h-full object-cover"
-                            />
-                        </div>
+                        <Image
+                            key={image._key}
+                            width={image.asset.metadata.dimensions.width}
+                            height={image.asset.metadata.dimensions.height}
+                            src={image.asset.url}
+                            alt={`Gallery Image ${image._key}`}
+                            className={`w-full h-dvw md:h-full object-cover ${image.width === 4 ? "md:col-span-4" : image.width === 8 ? "md:col-span-8" : "md:col-span-12"}`}
+                        />
                     ))}
                 </div>
             </div>
